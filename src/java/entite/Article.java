1fe56c87672ea3b9c4045a1b007357200e02d6b7
Adrianavony <andrianavony@gmail.com>
2015-03-22 18:34:48 +0100

    step 11 enregistre modification Article

--- src/java/entite/Article.java
+++ src/java/entite/Article.java
@@ -6,6 +6,7 @@
 package entite;
 
 import java.io.Serializable;
+import java.util.Collection;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -16,19 +17,26 @@ import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
  * @author S.ANDRIANAVONY
  */
 @Entity
+@Table(name = "article")
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Article.findAll", query = "SELECT a FROM Article a"),
-    @NamedQuery(name = "Article.findByIdarticle", query = "SELECT a FROM Article a WHERE a.idarticle = :idarticle")})
+    @NamedQuery(name = "Article.findByIdarticle", query = "SELECT a FROM Article a WHERE a.idarticle = :idarticle"),
+    @NamedQuery(name = "Article.findByDescription", query = "SELECT a FROM Article a WHERE a.description = :description"),
+    @NamedQuery(name = "Article.findByArticlename", query = "SELECT a FROM Article a WHERE a.articlename = :articlename"),
+    @NamedQuery(name = "Article.findByOfficialname", query = "SELECT a FROM Article a WHERE a.officialname = :officialname")})
 public class Article implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -37,21 +45,38 @@ public class Article implements Serializable {
     @Size(min = 1, max = 50)
     @Column(nullable = false, length = 50)
     private String idarticle;
-    @JoinColumn(name = "IDGENERATION", referencedColumnName = "IDGENERATION")
-    @ManyToOne
-    private Generation idgeneration;
-    @JoinColumn(name = "IDSPECIE", referencedColumnName = "IDSPECIE", insertable = false, updatable = false)
-    @ManyToOne
-    private Specie idspecie;
+    @Size(max = 50)
+    @Column(length = 50)
+    private String description;
+    @Size(max = 50)
+    @Column(length = 50)
+    private String articlename;
+    @Size(max = 50)
+    @Column(length = 50)
+    private String officialname;
+    @OneToMany(mappedBy = "idarticle")
+    private Collection<Trace> traceCollection;
+    @OneToMany(mappedBy = "idarticle")
+    private Collection<Sampleplangroup> sampleplangroupCollection;
+    @OneToMany(mappedBy = "idarticle")
+    private Collection<Batch> batchCollection;
     @JoinColumn(name = "IDSTAGE", referencedColumnName = "IDSTAGE")
     @ManyToOne
     private Stage idstage;
-     @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "IDGENERATION", referencedColumnName = "IDGENERATION")
+    @ManyToOne
+    private Generation idgeneration;
+    @JoinColumn(name = "IDSPECIE", referencedColumnName = "IDSPECIE")
+    @ManyToOne
+    private Specie idspecie;
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumns({
         @JoinColumn( name="idvariety", referencedColumnName = "IDVARIETY", insertable = true, updatable = false, nullable = true),
         @JoinColumn( name="idspecie", referencedColumnName = "IDSPECIE", insertable = true, updatable = false, nullable = true)
     })
     private Variety idvariety;
+    @OneToMany(mappedBy = "idarticle")
+    private Collection<Samples> samplesCollection;
 
     public Article() {
     }
@@ -66,6 +91,65 @@ public class Article implements Serializable {
 
     public void setIdarticle(String idarticle) {
         this.idarticle = idarticle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getArticlename() {
+        return articlename;
+    }
+
+    public void setArticlename(String articlename) {
+        this.articlename = articlename;
+    }
+
+    public String getOfficialname() {
+        return officialname;
+    }
+
+    public void setOfficialname(String officialname) {
+        this.officialname = officialname;
+    }
+
+    @XmlTransient
+    public Collection<Trace> getTraceCollection() {
+        return traceCollection;
+    }
+
+    public void setTraceCollection(Collection<Trace> traceCollection) {
+        this.traceCollection = traceCollection;
+    }
+
+    @XmlTransient
+    public Collection<Sampleplangroup> getSampleplangroupCollection() {
+        return sampleplangroupCollection;
+    }
+
+    public void setSampleplangroupCollection(Collection<Sampleplangroup> sampleplangroupCollection) {
+        this.sampleplangroupCollection = sampleplangroupCollection;
+    }
+
+    @XmlTransient
+    public Collection<Batch> getBatchCollection() {
+        return batchCollection;
+    }
+
+    public void setBatchCollection(Collection<Batch> batchCollection) {
+        this.batchCollection = batchCollection;
+    }
+
+    public Stage getIdstage() {
+        return idstage;
+    }
+
+    public void setIdstage(Stage idstage) {
+        this.idstage = idstage;
     }
 
     public Generation getIdgeneration() {
@@ -84,20 +168,21 @@ public class Article implements Serializable {
         this.idspecie = idspecie;
     }
 
-    public Stage getIdstage() {
-        return idstage;
-    }
-
-    public void setIdstage(Stage idstage) {
-        this.idstage = idstage;
-    }
-
     public Variety getIdvariety() {
         return idvariety;
     }
 
     public void setIdvariety(Variety idvariety) {
         this.idvariety = idvariety;
+    }
+
+    @XmlTransient
+    public Collection<Samples> getSamplesCollection() {
+        return samplesCollection;
+    }
+
+    public void setSamplesCollection(Collection<Samples> samplesCollection) {
+        this.samplesCollection = samplesCollection;
     }
 
     @Override
